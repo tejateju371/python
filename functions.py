d8a6c6bccad4b6034db9ddb53cc89819e7c092ee
teju <tejuteja371@gmail.com>
2024-09-12 22:55:08 +0530

    bye dude

---
+++ functions.py
@@ -0,0 +1,15 @@
+def callme(name,age):
+     print( f"name is {name} and age is{age}")
+
+
+callme("teju",24)
+callme("raju",28)
+callme("bajju",18)
+# sum 
+def sum(num1,num2):
+     return num1+num2
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+
+ans=sum(num1,num2)
+print(ans)
