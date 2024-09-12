38d6a247e5edc8fe78022a498bd9ade108b592ec
teju <tejuteja371@gmail.com>
2024-09-12 21:41:57 +0530

    arrays

---
+++ arrays.py
@@ -0,0 +1,10 @@
+animals=['cat','dog','lion','ass','bear']
+animals.sort()
+for i in animals:
+    print(i)
+print(" thid is reverse")
+print(type (animals) )  
+
+animals.reverse()
+for i in animals:
+    print(i)
